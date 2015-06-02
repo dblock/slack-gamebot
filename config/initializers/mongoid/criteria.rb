b992a555bc665dcf8290198658a308be784a615c
dblock <dblock@dblock.org>
2015-06-02 09:12:59 -0400

    Get rid of the mongoid criteria hack and private Giphy branch.

--- config/initializers/mongoid/criteria.rb
+++
@@ -1,8 +0,0 @@
-module Mongoid
-  class Criteria
-    def respond_to?(method, include_private = false)
-      # see https://github.com/intridea/grape/blob/master/lib/grape/dsl/inside_route.rb#L209, causes a merge without options
-      method == 'merge' ? false : super
-    end
-  end
-end
