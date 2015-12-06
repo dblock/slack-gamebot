bc66931a77cf82ccf64ee0ef3aa7a02b7967e63f
Andy Poirier <andpoi@gmail.com>
2015-12-06 12:15:27 -0500

    creating a command to change gifs status
    type gamebot switchgifs to change whether gifs are displayed with
    comments or not.

---
+++ slack-gamebot/commands/switchgifs.rb
@@ -0,0 +1,18 @@
+module SlackGamebot
+  module Commands
+    class Switchgifs < SlackRubyBot::Commands::Base
+      def self.call(client, data, _match)
+        if ENV[GAMEBOT_ENABLE_GIFS] = true
+                    message = "Thanks <@#{data.user}>! Gifs are off."
+                    ENV[GAMEBOT_ENABLE_GIFS] = false
+                  else
+                    message = "Thanks <@#{data.user}>! Gifs are on."
+                    ENV[GAMEBOT_ENABLE_GIFS] = true
+        end
+        send_message_with_gif client, data.channel, message, 'welcome'
+        #logger.info "DISABLEGIFS: #{data.user}"
+        user
+      end
+    end
+  end
+end
