3ec1a4b72a4349d6e219d4a7b4a60c77b30e6805
Andy Poirier <andpoi@gmail.com>
2015-12-06 12:56:28 -0500

    requiring gamebot_secret for updating gifs

--- slack-gamebot/commands/switchgifs.rb
+++ slack-gamebot/commands/switchgifs.rb
@@ -2,11 +2,11 @@ module SlackGamebot
   module Commands
     class Switchgifs < SlackRubyBot::Commands::Base
       def self.call(client, data, _match)
-        # fail ArgumentError, "Missing ENV['GAMEBOT_SECRET']." unless SlackGamebot.config.secret.present?
-        # arguments = match['expression'].split.reject(&:blank?) if match.names.include?('expression')
-        # secret = arguments.first if arguments
-        # fail ArgumentError, 'Missing secret.' unless secret.present?
-        # fail ArgumentError, 'Invalid secret.' unless secret == SlackGamebot.config.secret
+        fail ArgumentError, "Missing ENV['GAMEBOT_SECRET']." unless SlackGamebot.config.secret.present?
+        arguments = match['expression'].split.reject(&:blank?) if match.names.include?('expression')
+        secret = arguments.first if arguments
+        fail ArgumentError, 'Missing secret.' unless secret.present?
+        fail ArgumentError, 'Invalid secret.' unless secret == SlackGamebot.config.secret
         if SlackGamebot.config.enable_gifs == true
           message = "Thanks <@#{data.user}>! Gifs are off."
           SlackGamebot.config.enable_gifs = false
@@ -16,7 +16,6 @@ module SlackGamebot
         end
         send_message_with_gif client, data.channel, message, 'welcome'
         # logger.info "DISABLEGIFS: #{data.user}"
-        user
       end
     end
   end
