ffbd4d547ac9f881146a032e73aa2f7a21a10f95
Andy Poirier <andpoi@gmail.com>
2015-12-06 12:36:53 -0500

    issues

--- slack-gamebot/commands/switchgifs.rb
+++ slack-gamebot/commands/switchgifs.rb
@@ -2,12 +2,12 @@ module SlackGamebot
   module Commands
     class Switchgifs < SlackRubyBot::Commands::Base
       def self.call(client, data, _match)
-        if ENV[GAMEBOT_ENABLE_GIFS] == true
+        if GAMEBOT_ENABLE_GIFS == true
           message = "Thanks <@#{data.user}>! Gifs are off."
-          ENV[GAMEBOT_ENABLE_GIFS] = false
+          GAMEBOT_ENABLE_GIFS = false
         else
           message = "Thanks <@#{data.user}>! Gifs are on."
-          ENV[GAMEBOT_ENABLE_GIFS] = true
+          GAMEBOT_ENABLE_GIFS = true
         end
         send_message_with_gif client, data.channel, message, 'welcome'
         # logger.info "DISABLEGIFS: #{data.user}"
