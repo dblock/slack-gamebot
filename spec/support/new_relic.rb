0ee386edb51e4b98ed4f2af528839944c1ef5676
dblock <dblock@dblock.org>
2015-12-30 10:05:38 -0500

    Slightly pruned newrelic.yml and destroying log after test suite.

--- spec/support/new_relic.rb
+++ spec/support/new_relic.rb
@@ -4,4 +4,7 @@ RSpec.configure do |config|
   config.before :suite do
     NewRelic::Agent.manual_start
   end
+  config.after :suite do
+    FileUtils.rm_rf File.expand_path('log/newrelic_agent.log')
+  end
 end
