e142b711c68879ad9ff23a5f0154eeccc540f485
dblock <dblock@dblock.org>
2016-03-06 16:03:49 -0500

    Upgraded slack-ruby-bot and slack-ruby-client to 0.7.0.

--- config/initializers/slack-ruby-bot/client.rb
+++ config/initializers/slack-ruby-bot/client.rb
@@ -2,11 +2,9 @@ module SlackRubyBot
   class Client < Slack::RealTime::Client
     # keep track of the team that the client is connected to
     attr_accessor :owner
-
-    alias_method :_build_socket, :build_socket
-    def build_socket
-      @store_class = Slack::RealTime::Stores::Starter
-      _build_socket
-    end
   end
+end
+
+Slack::RealTime::Client.configure do |config|
+  config.store_class = Slack::RealTime::Stores::Starter
 end
