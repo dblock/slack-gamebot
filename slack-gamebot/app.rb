dd89eb86b4b68819c9ecdd61fbd8980b1dd68a64
dblock <dblock@dblock.org>
2016-07-14 13:16:13 -0400

    Fix: typo in dead bot message. [ci skip]

--- slack-gamebot/app.rb
+++ slack-gamebot/app.rb
@@ -2,7 +2,7 @@ module SlackGamebot
   class App < SlackRubyBotServer::App
     DEAD_MESSAGE = <<-EOS
 This leaderboard has been dead for over a month, deactivating.
-Re-install the bot at https://www.playplay.io. Your data will be purged in 2 weeks.'
+Re-install the bot at https://www.playplay.io. Your data will be purged in 2 weeks.
 EOS
 
     def prepare!
