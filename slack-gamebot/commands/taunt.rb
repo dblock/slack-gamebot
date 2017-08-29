3826f49a7e9902b403ce53c45d25d5ed94e56de4
Marin Hyatt <marin.hyatt@gmail.com>
2017-08-28 22:09:22 -0400

    Fixed Taunt command and spec
    Got rid of the Taunt class mentioned and fixed the spec so there is no (hidden)
    failure. Revised the Taunt command to support all game names. Changed the taunt
    to refer to the present tense.

--- slack-gamebot/commands/taunt.rb
+++ slack-gamebot/commands/taunt.rb
@@ -3,16 +3,21 @@ module SlackGamebot
     class Taunt < SlackRubyBot::Commands::Base
       def self.call(client, data, match)
         taunter = ::User.find_create_or_update_by_slack_id!(client, data.user)
-        if taunter.registered?
-          arguments = match['expression'].split.reject(&:blank?) if match['expression']
-          arguments ||= []
-          taunt = ::Taunt.create_from_teammates_and_opponents!(client.owner, data.channel, taunter, arguments)
-          client.say(channel: data.channel, text: "#{taunt.taunters.map(&:user_name).and} said #{taunt.map(&:user_name).and} sucks at ping pong!")
-          logger.info "TAUNT: #{client.owner} - #{taunter.user_name}"
+        arguments = match['expression'].split.reject(&:blank?) if match['expression']
+        arguments ||= []
+        if arguments.length > 1
+          client.say(channel: data.channel, text: 'Please only provide one user name to taunt.')
+        elsif arguments.length < 1
+          client.say(channel: data.channel, text: 'Please provide a user name to taunt.')
         else
-          client.say(channel: data.channel, text: "You aren't registered to play, please _register_ first.", gif: 'register')
-          logger.info "TAUNT: #{client.owner} - #{taunter.user_name}, failed, not registered"
-
+          victim = ::User.find_by_slack_mention!(client.owner, arguments.first)
+          if taunter.registered?
+            client.say(channel: data.channel, text: "#{taunter.user_name} says that #{victim.user_name} sucks at #{client.owner.game.name}!")
+            logger.info "TAUNT: #{client.owner} - #{taunter.user_name}"
+          else
+            client.say(channel: data.channel, text: "You aren't registered to play, please _register_ first.", gif: 'register')
+            logger.info "TAUNT: #{client.owner} - #{taunter.user_name}, failed, not registered"
+          end
         end
       end
     end
--- spec/slack-gamebot/commands/taunt_spec.rb
+++ spec/slack-gamebot/commands/taunt_spec.rb
@@ -1,16 +1,18 @@
 require 'spec_helper'
 
 describe SlackGamebot::Commands::Taunt, vcr: { cassette_name: 'user_info' } do
-  let!(:team) { Fabricate(:team) }
+  let!(:team) { Fabricate(:team, name: 'teamname') }
   let(:app) { SlackGamebot::Server.new(team: team) }
   let(:client) { app.send(:client) }
   let(:user) { Fabricate(:user, user_name: 'username') }
-  let(:opponent) { Fabricate(:user) }
-  it 'creates a singles taunt by user name' do
-    expect do
-      expect(message: "#{SlackRubyBot.config.user} taunt #{opponent.user_name}", user: user.user_id, channel: 'pongbot').to respond_with_slack_message(
-        "#{user.user_name} said that #{opponent.user_name} sucks at ping pong!"
-      )
-    end
+  it 'taunts one person by user id' do
+    victim = Fabricate(:user, team: team)
+        expect(message: "#{SlackRubyBot.config.user} taunt <@#{victim.user_id}>", user: user.user_id).to respond_with_slack_message(
+      "#{user.user_name} says that #{victim.user_name} sucks at #{client.owner.game.name}!")
+  end
+  it 'taunts one person by user name' do
+    victim = Fabricate(:user, team: team)
+       expect(message: "#{SlackRubyBot.config.user} taunt #{victim.user_name}", user: user.user_id).to respond_with_slack_message(
+      "#{user.user_name} says that #{victim.user_name} sucks at #{client.owner.game.name}!")
   end
 end
