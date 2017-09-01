bedfd09a6046259a066db0d3443245e87f67c472
dblock <dblock@dblock.org>
2017-09-01 15:39:00 -0400

    Improved homepage layout.

--- spec/integration/index_spec.rb
+++ spec/integration/index_spec.rb
@@ -7,7 +7,7 @@ describe 'index.html', js: true, type: :feature do
   end
   it 'includes a link to add to slack with the client id' do
     expect(title).to eq('PlayPlay.io - Ping Pong Bot, Chess Bot, Pool Bot and Tic Tac Toe Bot for Slack')
-    click_link 'Get Started'
+    click_link 'Add to Slack'
     expect(first('a[class=add-to-slack]')['href']).to eq "https://slack.com/oauth/authorize?scope=bot&client_id=#{game.client_id}"
   end
 end
