812efd66f988d25baf5251403832a9f5836a1004
dblock <dblock@dblock.org>
2018-08-19 20:50:55 -0400

    Log socket activity.

---
+++ config/initializers/celluloid.rb
@@ -0,0 +1,71 @@
+require 'slack/real_time/concurrency/celluloid'
+require 'celluloid/internals/logger'
+
+module Slack
+  module RealTime
+    module Concurrency
+      module Celluloid
+        class Socket < Slack::RealTime::Socket
+          include ::Celluloid::Internals::Logger
+
+          def initialize(*args)
+            super
+            logger.level = Logger::INFO
+          end
+
+          def log_info(message)
+            if message == @message
+              @count += 1
+            else
+              if @message && @message.is_a?(Array) && @count && @count > 1
+                logger.info(@message.concat(["repeated #{@count} times"]))
+              end
+              @count = 1
+              @message = message
+              logger.info(message)
+            end
+          end
+
+          def read
+            buffer = socket.readpartial(BLOCK_SIZE)
+            log_info(["got a nil buffer"]) unless buffer
+            log_info(["got an empty buffer"]) if buffer && buffer.size == 0
+            async.handle_read(buffer)
+          end
+
+          def build_driver
+            @logger = Logger.new(STDOUT)
+            @logger.level = Logger::INFO
+            ::WebSocket::Driver.client(self).tap do |ws|
+              ws.on :open do
+                log_info [ws.object_id, :server_open]
+              end
+
+              ws.on :message do |message|
+                log_info [ws.object_id, :server_message, message.data]
+              end
+
+              ws.on :close do |close|
+                log_info [ws.object_id, :server_close, close.code, close.reason]
+              end
+
+              ws.on :error do |error|
+                log_info [ws.object_id, :server_error, error.message]
+              end
+
+              ws.on :ping do |ping|
+                log_info [ws.object_id, :server_ping, ping.data]
+              end
+
+              ws.on :pong do |pong|
+                log_info [ws.object_id, :server_pong, pong.data]
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+
